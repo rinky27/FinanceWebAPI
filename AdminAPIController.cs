3c21e9ac179377f4e89a81561161c3dfde2466e2
rinky27 <rinky.singh@lntinfotech.com>
2021-04-11 19:20:07 +0530

    Add files via upload

---
+++ AdminAPIController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using FinanceManagementSystem.Models;
+
+namespace FinanceManagementSystem.Controllers
+{
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [Route("api/AdminAPI")]
+    public class AdminAPIController : ApiController
+    {
+        FinanceEntities db = new FinanceEntities();
+        [Route("api/AdminAPI/GetAllCustomers")]
+        [HttpGet]
+    
+        public IEnumerable<Customer> Get()
+        {
+            try
+            {
+                return db.Customers.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        [Route("api/AdminAPI/GetCustomerByID/{id}")]
+
+        [HttpGet]
+        public Customer Get(int id)
+        {
+            try
+            {
+                var data = db.Customers.Where(x => x.RegNumber == id).SingleOrDefault();
+                if (data == null)
+                    throw new Exception("Invalid RegNumber");
+                else
+                    return data;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+
+        }
+        [Route("api/AdminAPI/AdminLogin/{name}/{pwd}")]
+
+        [HttpGet]
+        public string Get(string name, string pwd)
+        {
+            string result = "";
+            try
+            {
+                var data = db.AdminCredentials.Where(x => x.AdminUsername == name && x.AdminPassword ==pwd);
+                if (data.Count() == 0)
+                    result = "Inavalid credentials";
+                else
+                    result = "Login Successful";
+            }
+            catch (Exception e)
+            {
+                throw e;
+
+            }
+            return result;
+        }
+    
+    }
+    }
---
+++ LoginAPIController.cs
@@ -0,0 +1,38 @@
+﻿using FinanceManagementSystem;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using FinanceManagementSystem.Models;
+using System;
+
+namespace FinanceManagementSystem.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [Route("api/LoginAPI")]
+    public class LoginAPIController : ApiController
+    {
+        FinanceEntities db = new FinanceEntities();
+        [Route("api/LoginAPI/Login/{name}/{pwd}")]
+        [HttpGet]
+        public bool Get(string userName, string userPwd)
+        {
+            bool result;
+            try
+            {
+                var data = db.Customers.Where(x => x.CustUsername == userName && x.CustPassword == userPwd);
+                if (data.Count() == 0)
+                    result = true;
+                else
+                    result = false;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return result;
+        }
+    }
+}
